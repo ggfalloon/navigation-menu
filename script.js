eeee2d7860f4fdae36df8f291f0190b06f0f0363
Gabriele Falloon <ggfalloon@gmail.com>
2021-03-30 14:39:30 -0500

    when menu item clicked, dropdown is hidden

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
 
+    // Display dropdown menu when inventory is clicked
     $("#droplink").on("click", function () {
         $(".drop-content").css("display", "block");
-        // $("#droplink").css("border", "none");
     });
+
+    // When a menu item is clicked, dropdown is hidden
+
+    $(".drop-content").on("click", function () {
+        $(".drop-content").css("display", "none");
+    })
 
 });
 
